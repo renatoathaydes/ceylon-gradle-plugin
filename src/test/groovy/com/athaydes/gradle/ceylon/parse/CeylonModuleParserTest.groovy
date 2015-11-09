4666bb73a0ea4161e3f2128bafe7a3fbe4ded9eb
renatoathaydes <renatoathaydes@hotmail.com>
2015-11-09 21:33:52 +0100

    Removed comments-removal tests and added tests for new expected behaviour

--- src/test/groovy/com/athaydes/gradle/ceylon/parse/CeylonModuleParserTest.groovy
+++ src/test/groovy/com/athaydes/gradle/ceylon/parse/CeylonModuleParserTest.groovy
@@ -7,62 +7,55 @@ class CeylonModuleParserTest {
     def parser = new CeylonModuleParser()
 
     @Test
-    void "Can remove comments from each line"() {
-        assert parser.nonCommentsFrom( '//hello', [ ] ) == [ ]
-        assert parser.nonCommentsFrom( ' // hello', [ ] ) == [ ]
-        assert parser.nonCommentsFrom( 'hello', [ ] ) == [ 'hello' ]
-        assert parser.nonCommentsFrom( 'hello // bye', [ ] ) == [ 'hello' ]
-        assert parser.nonCommentsFrom( 'hello /* this is  a comment */ hi', [ ] ) == [ 'hello', 'hi' ]
-        assert parser.nonCommentsFrom( 'hello /*this*/is/*comment*/bye', [ ] ) == [ 'hello', 'is', 'bye' ]
+    void "Can parse module with multi-line block comment"() {
+        def result = parser.parse( 'example.ceylon', """
+        /* a comment
+           spanning several lines
+           Some [[Example]] markup.
+           And even some code:
+           <code>shared function() {
+               print("Hello world!");
+           }
+           </code>
+        */
+        module my.test.module {
+           // this is an import
+           import other.module "1.0";
+        }
+        """ )
+
+        assert result
+        assert result.moduleName == 'my.test.module'
+        assert result.imports == [ [ module: 'other.module', version: '1.0' ] ]
     }
 
     @Test
-    void "Can remove multi-line block comment"() {
-        def result = [ ]
-        def go = parser.&nonCommentsFrom.rcurry( result )
-        go ''
-        go '        /* a comment'
-        go '           spanning several lines'
-        go '           Some [[Example]] markup.'
-        go '           And even some code:'
-        go '           <code>shared function() {'
-        go '               print("Hello world!");'
-        go '           }'
-        go '           </code>'
-        go '        */'
-        go '        module my.test.module {'
-        go '           // this is an import'
-        go '           import other.module "1.0";'
-        go '        }'
+    void "Can parse module with multi-line doc comment with block comment inside it"() {
+        def result = parser.parse( 'example.ceylon', """
+        "A comment
+         spanning several lines
+         Some [[Example]] markup.
+         And even some code:
+             shared function() {
+                 print(\\"Hello world!\\");
+             }
 
-        assert result == [ 'module my.test.module {', 'import other.module "1.0";', '}' ]
-    }
+          /* block comments
+             are acceptable inside doc comments.
+           */
+         "
+        module my.test.module "0" {
+           "This import is required.
+            The \\"1.0\\" is the version""
+            "
+           import other.module "1.0";
+        }
+        """ )
 
-    @Test
-    void "Can remove multi-line doc comment"() {
-        def result = [ ]
-        def go = parser.&nonCommentsFrom.rcurry( result )
-        go ''
-        go '        "A comment'
-        go '         spanning several lines'
-        go '         Some [[Example]] markup.'
-        go '         And even some code:'
-        go '             shared function() {'
-        go '                 print(\\"Hello world!\\");'
-        go '             }'
-        go '          '
-        go '          /* block comments'
-        go '             are acceptable inside doc comments.'
-        go '           */'
-        go '         "'
-        go '        module my.test.module {'
-        go '           "This import is required.'
-        go '            The \\"1.0\\" is the version""'
-        go '            "'
-        go '           import other.module "1.0";'
-        go '        }'
-
-        assert result == [ 'module my.test.module {', 'import other.module "1.0";', '}' ]
+        assert result
+        assert result.moduleName == 'my.test.module'
+        assert result.version == '0'
+        assert result.imports == [ [ name: 'other.module', version: '1.0' ] ]
     }
 
     @Test
@@ -80,6 +73,16 @@ class CeylonModuleParserTest {
         assert result.imports == [
                 [ name: 'java.lang.base', version: '7' ]
         ]
+    }
+
+    @Test
+    void "Can parse a simple module file without any imports"() {
+        def result = parser.parse 'myfile.ceylon', 'module com.hello.world "1.0" {}'
+
+        assert result
+        assert result.moduleName == 'com.hello.world'
+        assert result.version == '1.0'
+        assert result.imports == null
     }
 
     @Test
@@ -129,6 +132,53 @@ class CeylonModuleParserTest {
         assert result.version == '1.0.0'
         assert result.imports == [
                 [ name: 'java.lang.base', version: '7' ]
+        ]
+    }
+
+    @Test
+    void "Can parse a module file with Maven imports"() {
+        def result = parser.parse 'myfile.ceylon', """
+                module com.hello.world "1.0" {
+                    import "org.apache.logging.log4j:log4j-core"  "2.4.1";
+                    import "org.apache.logging.log4j:log4j-api"  "2.4.1";
+                    import "org.junit:junit"  "4.12";
+                    import ceylon.collection "1.2";
+                }
+                """
+
+        assert result
+        assert result.moduleName == 'com.hello.world'
+        assert result.version == '1.0'
+        assert result.imports == [
+                [ name: 'org.apache.logging.log4j:log4j-core', version: '2.4.1' ],
+                [ name: 'org.apache.logging.log4j:log4j-api', version: '2.4.1' ],
+                [ name: 'org.junit:junit', version: '4.12' ],
+                [ name: 'ceylon.collection', version: '1.2' ]
+        ]
+    }
+
+    @Test
+    void "Can parse a module file with annotated imports and module"() {
+        def result = parser.parse 'myfile.ceylon', """
+                "An example module
+                 with \"lots\" of annotations"
+                shared("jvm")
+                module com.hello.world "1.0.0" {
+                    shared import java.lang.base  "7";
+                    shared("js") import org.jquery "2.5";
+                    // normal import
+                    import ceylon.promise "1.2";
+                }
+                """
+
+        assert result
+        assert result.moduleName == 'com.hello.world'
+        assert result.version == '1.0.0'
+        assert result.shared == true
+        assert result.imports == [
+                [ name: 'java.lang.base', version: '7', shared: true ],
+                [ name: 'org.jquery', version: '2.5', shared: true ],
+                [ name: 'ceylon.promise', version: '1.2' ],
         ]
     }
 
