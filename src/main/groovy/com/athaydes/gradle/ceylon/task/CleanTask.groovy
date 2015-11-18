081b54079ff626b5293025bfb4039955ffdfdf13
renatoathaydes <renatoathaydes@hotmail.com>
2015-11-18 20:26:10 +0100

    Added compileCeylon task's outputs to inputs of clean task

--- src/main/groovy/com/athaydes/gradle/ceylon/task/CleanTask.groovy
+++ src/main/groovy/com/athaydes/gradle/ceylon/task/CleanTask.groovy
@@ -2,19 +2,12 @@ package com.athaydes.gradle.ceylon.task
 
 import com.athaydes.gradle.ceylon.CeylonConfig
 import org.gradle.api.Project
-import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
 
 class CleanTask {
 
-    static final Logger log = Logging.getLogger( CleanTask )
-
     static List inputs( Project project, CeylonConfig config ) {
-        def result = GenerateOverridesFileTask.outputs( project, config )
-
-        log.info( "The clean task has the following inputs: $result" )
-
-        return result
+        GenerateOverridesFileTask.outputs( project, config ) +
+                CompileCeylonTask.outputs( project, config )
     }
 
 }
