61a4284eeb728c9a730f2a916a40ccfa133d3e9e
renatoathaydes <renatoathaydes@hotmail.com>
2015-11-19 19:54:50 +0100

    Improved importJars task

--- src/main/groovy/com/athaydes/gradle/ceylon/task/ImportJarsTask.groovy
+++ src/main/groovy/com/athaydes/gradle/ceylon/task/ImportJarsTask.groovy
@@ -4,10 +4,10 @@ import com.athaydes.gradle.ceylon.CeylonConfig
 import com.athaydes.gradle.ceylon.util.CeylonRunner
 import org.gradle.api.GradleException
 import org.gradle.api.Project
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier
-import org.gradle.api.artifacts.result.ResolvedDependencyResult
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
+import org.gradle.internal.component.external.model.ModuleComponentArtifactIdentifier
 
 class ImportJarsTask {
 
@@ -27,43 +27,37 @@ class ImportJarsTask {
 
         log.debug "Importing jars"
 
-        def dependencyTree = ResolveCeylonDependenciesTask.dependencyTreeOf( project )
-
-        CeylonRunner.withCeylon( project, config ) { File ceylon ->
-            dependencyTree.allResolvedTransitiveDependencies.each { dep ->
-                importDependency ceylon, dep, project, config
-            }
-        }
-    }
-
-    static void importDependency( File ceylon,
-                                  ResolvedDependencyResult dependency,
-                                  Project project,
-                                  CeylonConfig config ) {
-        log.debug( "Will try to install ${dependency.selected} into the Ceylon repository" )
-
         def repo = project.file( config.output )
 
         if ( !repo.directory && !repo.mkdirs() ) {
             throw new GradleException( "Output repository does not exist and cannot be created ${repo.absolutePath}." )
         }
 
-        final depId = dependency.selected.id
-        if ( depId instanceof ModuleComponentIdentifier ) {
-            def module = "${depId.group}:${depId.module}/${depId.version}"
-            def jarFile = project.configurations.getByName( 'ceylonCompile' )
+        CeylonRunner.withCeylon( project, config ) { File ceylon ->
+            project.configurations.ceylonCompile
                     .resolvedConfiguration
-                    .resolvedArtifacts.find {
-                def id = it.id.componentIdentifier
-                id instanceof ModuleComponentIdentifier &&
-                        id.group == depId.group &&
-                        id.module == depId.module &&
-                        id.version == depId.version
-            }?.file
+                    .resolvedArtifacts.each { artifact ->
+                importDependency ceylon, repo, artifact, project
+            }
+        }
+    }
+
+    static void importDependency( File ceylon,
+                                  File repo,
+                                  ResolvedArtifact artifact,
+                                  Project project ) {
+        log.debug( "Will try to install ${artifact} into the Ceylon repository" )
+
+        def jarFile = artifact.file
+        def artifactId = artifact.id
+
+        if ( artifactId instanceof ModuleComponentArtifactIdentifier ) {
+            def id = artifactId.componentIdentifier
+            def module = "${id.group}:${id.module}/${id.version}"
 
             if ( jarFile?.exists() ) {
                 def command = "${ceylon.absolutePath} import-jar --force " +
-                        "--out=${repo.absolutePath} $module ${jarFile.absolutePath}"
+                        "--out=${repo.absolutePath} ${module} ${jarFile.absolutePath}"
 
                 log.info "Running command: $command"
                 def process = command.execute( [ ], project.file( '.' ) )
@@ -73,6 +67,8 @@ class ImportJarsTask {
                 throw new GradleException( "Dependency ${module} could not be installed in the Ceylon Repository" +
                         " because its jarFile could not be located: ${jarFile}" )
             }
+        } else {
+            log.warn( "Artifact being ignored as it is not a module artifact: ${artifact}" )
         }
     }
 
